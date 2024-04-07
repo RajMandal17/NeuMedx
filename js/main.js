e5dfc32b5329fb61f0746da50f18f99d20aca6da
Rajkumar <Rajmandal147@gmail.com>
2024-04-07 14:34:38 +0530

    neumedx

--- js/main.js
+++ js/main.js
@@ -84,6 +84,6 @@ function kpengine() {
 
 
 function healthSupport() {
-    window.open("https://healthsupport247.in/#", "_blank");
+    window.open("https://healthCheckup247.up.railway.app/", "_blank");
 }
 
